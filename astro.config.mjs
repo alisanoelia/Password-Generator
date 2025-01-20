a1944cec74a49eeb7e044b3365aa13bc540c16d3
asha <asha>
2025-01-20 16:06:47 -0500

    [fix]: error .git to .github

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alisanoelia.github.io/password-generator',
-  base: 'https://github.com/alisanoelia/password-generator.git',
+  base: 'password-generator',
   integrations: [tailwind(), react()]
 })
